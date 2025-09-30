efde0eac4307cb15c5161e8f17a15687714ebc5a
Rupeshkotha <roopeshkotha@gmail.com>
2025-09-30 23:00:11 +0530

    added user.py for login and register

---
+++ user.py
@@ -0,0 +1,18 @@
+from flask_login import UserMixin
+
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data["_id"])
+        self.email = user_data["email"]
+        self.password = user_data["password"]
+
+    @staticmethod
+    def get(user_id):
+        from app import users_collection
+        user_data = users_collection.find_one({'_id': ObjectId(user_id)})
+        if user_data:
+            return User(user_data)
+        return None
+
+# We need ObjectId to query by _id in MongoDB
+from bson.objectid import ObjectId
